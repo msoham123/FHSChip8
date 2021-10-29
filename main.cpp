9a48e90561d8c83941e725d814e00593c5dd5164
msoham123 <msoham123@gmail.com>
2021-10-29 13:02:51 -0700

    Outline emulator loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "CPU/Core.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(int argc, char **arcgv) {
+    /* Pseudocode Implementation (Implement as development progresses)
+     *
+     *  // Set up render system and register input callbacks
+     *  setupGraphics();
+     *  setupInput();
+     *
+     *   // Initialize the Chip8 system and load the game into the memory
+     *   core.initialize();
+     *   core.loadGame("pong");
+     *
+     *   // Emulation loop
+     *   for(;;){
+     *   // Emulate one cycle
+     *   core.emulateCycle();
+     *   // If the draw flag is set, update the screen
+     *   if(myChip8.drawFlag)
+     *       drawGraphics();
+     *   }
+     *   // Store key press state (Press and Release)
+     *   core.setKeys();
+     *   }
+     */
+
+    Core core;
+
     return 0;
 }
