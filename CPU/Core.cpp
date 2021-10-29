4985913829b553287233fb0554b4df5cab2fcd2a
msoham123 <msoham123@gmail.com>
2021-10-29 15:56:00 -0700

    Implement fetch

--- CPU/Core.cpp
+++ CPU/Core.cpp
@@ -2,7 +2,6 @@
 
 void Core::initialize() {
 
-
 }
 
 void Core::emulateCycle() {
@@ -10,7 +9,9 @@ void Core::emulateCycle() {
 }
 
 void Core::fetch() {
-
+    // Remember that opcodes are 16 bit types
+    // Fetch 8-bit address, shift it by 8, and merge using OR bitwise operation
+    opcode = memory[programCounter] << 8 | memory[programCounter+1];
 }
 
 void Core::decode() {
