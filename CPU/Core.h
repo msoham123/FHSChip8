1d634492ac213da0d3a4afc9ef154ee648ad4017
msoham123 <msoham123@gmail.com>
2021-10-29 12:21:33 -0700

    Rearrange fields in Core

--- CPU/Core.h
+++ CPU/Core.h
@@ -4,23 +4,8 @@
 
 class Core {
 
-    // The Chip-8 has 35 opcodes, or 35 operations, each two bytes (16 bits) long
-    unsigned short opcode;
-
     // The Chip-8 has 4 kilobytes of memory
     unsigned char memory[4096];
-
-    /*
-     * The Chip-8 has 16 8-bit (one byte) general-purpose variable registers numbered
-     * 0 through F hexadecimal (0 through 15 in decimal) called V0 through VF
-     * */
-    unsigned char V[16];
-
-    // One 16-bit index register called I which is used to point at locations in memory
-    unsigned short I;
-
-    // A program counter, often called PC, which points at the current instruction in memory
-    unsigned short PC;
 
     /*
      * The display is 64 pixels wide and 32 pixels tall. Each pixel can be on or off.
@@ -28,13 +13,21 @@ class Core {
      * */
     unsigned char display[64 * 32];
 
+    // A program counter, often called PC, which points at the current instruction in memory
+    unsigned short PC;
 
+    // One 16-bit index register called I which is used to point at locations in memory
+    unsigned short I;
 
+    // The Chip-8 has 35 opcodes, or 35 operations, each two bytes (16 bits) long
+    unsigned short opcode;
 
-
-
+    /*
+     * The Chip-8 has 16 8-bit (one byte) general-purpose variable registers numbered
+     * 0 through F hexadecimal (0 through 15 in decimal) called V0 through VF
+     * */
+    unsigned char V[16];
 
 };
-
 
 #endif
