7edc9f3babf7bc53af4f0f1103cd02b346a23a08
msoham123 <msoham123@gmail.com>
2021-12-07 15:55:32 -0800

    Added FHS logo as replacement to four dots

--- src/main.cpp
+++ src/main.cpp
@@ -44,11 +44,65 @@ int main(int argc, char **arcgv) {
 
     setupGraphics();
 
-    // Example - Delete (sets 4 corners to white)
-    *(CORE.getDisplayArray() + 0) = true;
-    *(CORE.getDisplayArray() + 63) = true;
-    *(CORE.getDisplayArray() + 2047) = true;
-    *(CORE.getDisplayArray() + (2047 - 63)) = true;
+    // Example (Comment Out When Not Used) - Renders FHS
+
+    // F
+    *(CORE.getDisplayArray() + 263) = true;
+    *(CORE.getDisplayArray() + 264) = true;
+    *(CORE.getDisplayArray() + 265) = true;
+    *(CORE.getDisplayArray() + 266) = true;
+    *(CORE.getDisplayArray() + 267) = true;
+    *(CORE.getDisplayArray() + 263 + 64) = true;
+    *(CORE.getDisplayArray() + 263 + (64*2)) = true;
+    *(CORE.getDisplayArray() + 263 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 263 + (64 * 4)) = true;
+    *(CORE.getDisplayArray() + 263 + (64 * 5)) = true;
+    *(CORE.getDisplayArray() + 263 + (64 * 6)) = true;
+    *(CORE.getDisplayArray() + 264 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 265 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 266 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 267 + (64 * 3)) = true;
+
+    // H
+    *(CORE.getDisplayArray() + 272) = true;
+    *(CORE.getDisplayArray() + 272 + 64) = true;
+    *(CORE.getDisplayArray() + 272 + (64 * 2)) = true;
+    *(CORE.getDisplayArray() + 272 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 272 + (64 * 4)) = true;
+    *(CORE.getDisplayArray() + 272 + (64 * 5)) = true;
+    *(CORE.getDisplayArray() + 272 + (64 * 6)) = true;
+    *(CORE.getDisplayArray() + 273 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 274 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 275 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 276) = true;
+    *(CORE.getDisplayArray() + 276 + (64 * 1)) = true;
+    *(CORE.getDisplayArray() + 276 + (64 * 2)) = true;
+    *(CORE.getDisplayArray() + 276 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 276 + (64 * 4)) = true;
+    *(CORE.getDisplayArray() + 276 + (64 * 5)) = true;
+    *(CORE.getDisplayArray() + 276 + (64 * 6)) = true;
+
+
+    // S
+    *(CORE.getDisplayArray() + 281) = true;
+    *(CORE.getDisplayArray() + 282) = true;
+    *(CORE.getDisplayArray() + 283) = true;
+    *(CORE.getDisplayArray() + 284) = true;
+    *(CORE.getDisplayArray() + 285) = true;
+    *(CORE.getDisplayArray() + 281 + 64) = true;
+    *(CORE.getDisplayArray() + 281 + (64 * 2)) = true;
+    *(CORE.getDisplayArray() + 281 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 282 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 283 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 284 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 285 + (64 * 3)) = true;
+    *(CORE.getDisplayArray() + 285 + (64 * 4)) = true;
+    *(CORE.getDisplayArray() + 285 + (64 * 5)) = true;
+    *(CORE.getDisplayArray() + 285 + (64 * 6)) = true;
+    *(CORE.getDisplayArray() + 284 + (64 * 6)) = true;
+    *(CORE.getDisplayArray() + 283 + (64 * 6)) = true;
+    *(CORE.getDisplayArray() + 282 + (64 * 6)) = true;
+    *(CORE.getDisplayArray() + 281 + (64 * 6)) = true;
 
     // Keep the window open until forcefully closed
     bool running = true;
